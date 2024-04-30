9f6433ed794bb76c278636bb6127ebeea4e6ea1b
Omar Leiva <oleivahn@gmail.com>
2024-04-30 03:44:42 -0400

    QOL

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
-import ContactUs from "./contactUs/page";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
