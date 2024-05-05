a4f376f1d9b0a213cba507ab47af7eb9093f1021
Omar Leiva <oleivahn@gmail.com>
2024-05-05 02:19:10 -0400

    Fix word wrap from navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 import { siteConfig } from "@/config/site";
 import {
   Activity,
@@ -44,24 +48,23 @@ import { ThemeToggle } from "./theme-toggle";
 import { Icons } from "@/components/icons";
 
 export default function Navbar() {
+  // - How to set colors for active links
+  // https://stackoverflow.com/questions/68978743/tailwindcss-active-link-text-color-not-changing
+  const pathname = usePathname();
+
   return (
     <div className="flex w-full flex-col border-b">
       <header className="container sticky top-0 flex h-16 items-center gap-4  bg-background px-4 md:px-6">
-        {/* <div className="container"> */}
         <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
           <Link href="/" className="flex items-center space-x-2">
             <Icons.logo className="h-6 w-6" />
             <span className="inline-block font-bold">{siteConfig.name}</span>
           </Link>
           <Link
-            href="/"
-            className="text-foreground transition-colors hover:text-foreground"
-          >
-            Home
-          </Link>
-          <Link
             href="contactUs"
-            className="text-muted-foreground transition-colors hover:text-foreground w-[80px]"
+            className={`${
+              pathname === "/contactUs" ? "text-foreground" : ""
+            } text-muted-foreground transition-colors hover:text-foreground w-[80px] active:text-foreground`}
           >
             Contact Us
           </Link>
@@ -134,7 +137,7 @@ export default function Navbar() {
             </nav>
           </SheetContent>
         </Sheet>
-        <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+        <div className="flex items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
           <form className="ml-auto flex-1 sm:flex-initial">
             <div className="relative">
               <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
