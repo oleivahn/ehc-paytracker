a4ee696501d093758ca651b627d0bffb927412a4
Omar Leiva <oleivahn@gmail.com>
2024-05-10 04:04:08 -0400

    Routes

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -2,28 +2,28 @@ import { NextResponse } from "next/server";
 
 // - Sample GET request
 export const GET = async () => {
-  return NextResponse.json({ message: "Hello World!!!" }, { status: 200 });
-  // try {
-  //   return NextResponse.json({ message: "Hello World!!!" }, { status: 200 });
-  // } catch (error: unknown) {
-  //   const message = error instanceof Error ? error.message : "Unknown error";
+  // return NextResponse.json({ message: "Hello World!!!" }, { status: 200 });
+  try {
+    return NextResponse.json({ message: "Hello World!!!" }, { status: 200 });
+  } catch (error: unknown) {
+    const message = error instanceof Error ? error.message : "Unknown error";
 
-  //   return NextResponse.json({ message: message }, { status: 500 });
-  // }
+    return NextResponse.json({ message: message }, { status: 500 });
+  }
 };
 
 // - Sample POST request
 export const POST = async (request: Request, respose: NextResponse) => {
-  return NextResponse.json({ message: "Hello World!!!" }, { status: 200 });
-  // try {
-  //   // Info: You have to await the request.json() method to get the body of the request
-  //   const body = await request.json();
+  // return NextResponse.json({ message: "Hello World!!!" }, { status: 200 });
+  try {
+    // Info: You have to await the request.json() method to get the body of the request
+    const body = await request.json();
 
-  //   console.log("📗 LOG [ body ]:", body);
-  //   return NextResponse.json(body, { status: 200 });
-  // } catch (error: unknown) {
-  //   const message = error instanceof Error ? error.message : "Unknown error";
+    console.log("📗 LOG [ body ]:", body);
+    return NextResponse.json(body, { status: 200 });
+  } catch (error: unknown) {
+    const message = error instanceof Error ? error.message : "Unknown error";
 
-  //   return NextResponse.json({ message: message }, { status: 500 });
-  // }
+    return NextResponse.json({ message: message }, { status: 500 });
+  }
 };
