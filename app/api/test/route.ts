347a3d4ed50e5b133ecec62a36c6e7ade30f3350
Omar Leiva <oleivahn@gmail.com>
2024-05-10 03:39:31 -0400

    no routes

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,27 +1,27 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-// - Sample GET request
-export const GET = async () => {
-  try {
-    return NextResponse.json({ message: "Hello World!!!" }, { status: 200 });
-  } catch (error: unknown) {
-    const message = error instanceof Error ? error.message : "Unknown error";
+// // - Sample GET request
+// export const GET = async () => {
+//   try {
+//     return NextResponse.json({ message: "Hello World!!!" }, { status: 200 });
+//   } catch (error: unknown) {
+//     const message = error instanceof Error ? error.message : "Unknown error";
 
-    return NextResponse.json({ message: message }, { status: 500 });
-  }
-};
+//     return NextResponse.json({ message: message }, { status: 500 });
+//   }
+// };
 
-// - Sample POST request
-export const POST = async (request: Request, respose: NextResponse) => {
-  try {
-    // Info: You have to await the request.json() method to get the body of the request
-    const body = await request.json();
+// // - Sample POST request
+// export const POST = async (request: Request, respose: NextResponse) => {
+//   try {
+//     // Info: You have to await the request.json() method to get the body of the request
+//     const body = await request.json();
 
-    console.log("📗 LOG [ body ]:", body);
-    return NextResponse.json(body, { status: 200 });
-  } catch (error: unknown) {
-    const message = error instanceof Error ? error.message : "Unknown error";
+//     console.log("📗 LOG [ body ]:", body);
+//     return NextResponse.json(body, { status: 200 });
+//   } catch (error: unknown) {
+//     const message = error instanceof Error ? error.message : "Unknown error";
 
-    return NextResponse.json({ message: message }, { status: 500 });
-  }
-};
+//     return NextResponse.json({ message: message }, { status: 500 });
+//   }
+// };
