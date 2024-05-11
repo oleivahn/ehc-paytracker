fed3799e99cb65f778b4ee75d50f587eb2532fdd
Omar Leiva <oleivahn@gmail.com>
2024-05-11 02:37:17 -0400

    Current test

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 
 // - Sample GET request
 export const GET = async () => {
-  // return NextResponse.json({ message: "Hello World!!!" }, { status: 200 });
   try {
     return NextResponse.json({ message: "Hello World!!!" }, { status: 200 });
   } catch (error: unknown) {
@@ -14,12 +13,10 @@ export const GET = async () => {
 
 // - Sample POST request
 export const POST = async (request: Request, respose: NextResponse) => {
-  // return NextResponse.json({ message: "Hello World!!!" }, { status: 200 });
   try {
     // Info: You have to await the request.json() method to get the body of the request
     const body = await request.json();
 
-    console.log("📗 LOG [ body ]:", body);
     return NextResponse.json(body, { status: 200 });
   } catch (error: unknown) {
     const message = error instanceof Error ? error.message : "Unknown error";
