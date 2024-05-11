f4644aabf39a4f3579148bd5a251025d0e7e2770
Omar Leiva <oleivahn@gmail.com>
2024-05-11 01:35:38 -0400

    routes again

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -3,45 +3,45 @@ import connectDB from "@/lib/database-connection";
 import User from "@/models/user";
 
 export const GET = async () => {
-  return NextResponse.json({ message: "Hello World!!!" }, { status: 200 });
-  // try {
-  //   // - Connect to the database
-  //   await connectDB();
+  // return NextResponse.json({ message: "Hello World!!!" }, { status: 200 });
+  try {
+    // - Connect to the database
+    await connectDB();
 
-  //   const users = await User.find();
+    const users = await User.find();
 
-  //   return new NextResponse(JSON.stringify(users), { status: 200 });
-  // } catch (error: unknown) {
-  //   const message = error instanceof Error ? error.message : "Unknown error";
+    return new NextResponse(JSON.stringify(users), { status: 200 });
+  } catch (error: unknown) {
+    const message = error instanceof Error ? error.message : "Unknown error";
 
-  //   return NextResponse.json(
-  //     { message: "Error in fetching users", error: message },
-  //     { status: 500 }
-  //   );
-  // }
+    return NextResponse.json(
+      { message: "Error in fetching users", error: message },
+      { status: 500 }
+    );
+  }
 };
 
 export const POST = async (request: Request) => {
-  return NextResponse.json({ message: "Hello World!!!" }, { status: 200 });
-  // try {
-  //   const body = await request.json();
+  // return NextResponse.json({ message: "Hello World!!!" }, { status: 200 });
+  try {
+    const body = await request.json();
 
-  //   await connectDB();
+    await connectDB();
 
-  //   const newUser = new User(body);
-  //   const user = await newUser.save();
+    const newUser = new User(body);
+    const user = await newUser.save();
 
-  //   return new NextResponse(
-  //     JSON.stringify({ message: "User is created", user: newUser }),
-  //     { status: 201 }
-  //   );
-  // } catch (error: unknown) {
-  //   const message = error instanceof Error ? error.message : "Unknown error";
+    return new NextResponse(
+      JSON.stringify({ message: "User is created", user: newUser }),
+      { status: 201 }
+    );
+  } catch (error: unknown) {
+    const message = error instanceof Error ? error.message : "Unknown error";
 
-  //   console.log("There was an error creating a new user:", error.message);
-  //   return NextResponse.json(
-  //     { message: "Error creating user", error: message },
-  //     { status: 500 }
-  //   );
-  // }
+    console.log("There was an error creating a new user:", error.message);
+    return NextResponse.json(
+      { message: "Error creating user", error: message },
+      { status: 500 }
+    );
+  }
 };
