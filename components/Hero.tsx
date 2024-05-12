c954f915cb4d04eda8a140fdff9f92cce135cec2
Omar Leiva <oleivahn@gmail.com>
2024-05-11 23:39:19 -0400

    experience

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -61,7 +61,7 @@ const Hero = () => {
                   <div className="relative z-10 mb-4 flex items-center justify-center overflow-hidden bg-primary px-6 py-12 sm:mb-8 sm:h-[160px] sm:p-5 lg:mb-4 xl:mb-8">
                     <div>
                       <span className="block text-5xl font-extrabold text-white">
-                        09
+                        02
                       </span>
                       <span className="block text-base font-semibold text-white">
                         We have
