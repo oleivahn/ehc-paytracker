2ed28ee1504f21024110ff3a9825713bc5771943
Omar Leiva <oleivahn@gmail.com>
2025-10-07 21:53:28 -0400

    console log fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,6 +53,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
             "min-h-screen bg-background font-sans antialiased",
             fontSans.variable
           )}
+          suppressHydrationWarning={true}
         >
           <ThemeProvider
             attribute="class"
